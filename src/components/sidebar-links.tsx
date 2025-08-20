60c1d381ca189f2538ecc848148d4964d298cfb8
MohamedAli <mmkandeelz74@gmail.com>
2025-08-20 06:27:57 +0300

    feat(ui): add Sidebar component with navigation links

---
+++ src/components/sidebar-links.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import clsx from "clsx";
+import { Calendar, Grid, Settings, User, LucideIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+interface SidebarLinksProps {
+  link: {
+    label: string;
+    icon: string;
+    link: string;
+  };
+}
+const icons: Record<string, LucideIcon> = { Calendar, Grid, Settings, User };
+const SidebarLink = ({ link }: SidebarLinksProps) => {
+  const pathName = usePathname();
+  const isActive = pathName == link.link ? true : false;
+  const Icon = icons[link.icon];
+  return (
+    <Link href={link.link}>
+      <Icon
+        size={40}
+        className={clsx(
+          "stroke-gray-400  hover:stroke-violet-600 transition duration-200 ease-in-out",
+          isActive && "stroke-violet-600"
+        )}
+      />
+    </Link>
+  );
+};
+
+export default SidebarLink;
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,38 @@
+import SidebarLink from "./sidebar-links";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+const links = [
+  { label: "Home", icon: "Grid", link: "/home" },
+  { label: "Calendar", icon: "Calendar", link: "/Project" },
+  { label: "Profile", icon: "User", link: "/profile" },
+  { label: "Settings", icon: "Settings", link: "/settings" },
+];
+
+const Sidebar = () => {
+  return (
+    <Card className="py-5">
+      <CardHeader>
+        <CardTitle className="text-center">Card Title</CardTitle>
+        {/* <CardDescription>Card Description</CardDescription> */}
+      </CardHeader>
+      <CardContent>
+        <div className="w-full flex justify-center items-center gap-2">
+          {/* <Image
+          src={logo}
+          alt="Able logo"
+          priority
+          className="w-14"
+        /> */}
+        </div>
+        {links.map((link) => (
+          <SidebarLink key={link.link} link={link} />
+        ))}
+      </CardContent>
+      {/* <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter> */}
+    </Card>
+  );
+};
+
+export default Sidebar;
