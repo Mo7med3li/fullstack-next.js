455939ed37c3537e2c2b39129131d8b3a82ca19b
MohamedAli <mmkandeelz74@gmail.com>
2025-08-24 03:36:43 +0300

    feat(ui): create project card component

---
+++ src/lib/utils/date-format.ts
@@ -0,0 +1,8 @@
+const formatDate = (date: Date | string | number) =>
+  new Date(date).toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+export default formatDate;
